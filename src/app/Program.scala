024fb36ec0b426b8cc4a4985bb86f8c47a29dcde
Martin Hansen <m4h@outlook.dk>
2021-05-02 11:02:54 +0200

    Refactor Program

--- src/app/Program.scala
+++ src/app/Program.scala
@@ -1,36 +1,16 @@
 package app
 
-import app.models.{AicWeatherUpdate, AirlineWeatherUpdate, AtcWeatherUpdate, DiContainer}
+import app.models.{AicWeatherUpdate, AirlineWeatherUpdate, AtcWeatherUpdate, CanonicalWeatherUpdate}
 import com.rabbitmq.client.ConnectionFactory
 
 import scala.io.StdIn.readLine
 
 object Program extends App {
-
-  val diContainer = setupDi()
-
-  val factory = diContainer.connectionFactory
-  val connection = factory.newConnection()
-  val channel = connection.createChannel()
-  val senders = diContainer.senders
-  val enricher = diContainer.enricher
-  val scheduler = diContainer.scheduler
+  val scheduler = Scheduler(
+    "localhost",
+    "http://api.openweathermap.org/data/2.5/weather?zip=8260,dk&units=metric&appid=fb14f1139067845d2f30a3d147e7df89",
+    s => printf(s.queueName)
+  )
   scheduler.start(300)
   readLine()
-
-  private def setupDi(): DiContainer = {
-    DiContainer(
-      new ConnectionFactory() { setHost("localhost") },
-      senders = List(
-        MessageSender[AicWeatherUpdate](channel, "aic", new AicFilter, new AicTranslator),
-        MessageSender[AtcWeatherUpdate](channel, "atc", new AtcFilter, new AtcTranslator),
-        MessageSender[AirlineWeatherUpdate](channel, "sas", new AirlineFilter, new SasTranslator),
-        MessageSender[AirlineWeatherUpdate](channel, "klm", new AirlineFilter, new KlmTranslator),
-        MessageSender[AirlineWeatherUpdate](channel, "swa", new AirlineFilter, new XmlTranslator),
-        MessageSender[AirlineWeatherUpdate](channel, "baw", new AirlineFilter, new XmlTranslator),
-      ),
-      Enricher("http://api.openweathermap.org/data/2.5/weather?zip=8260,dk&units=metric&appid=fb14f1139067845d2f30a3d147e7df89"),
-      Scheduler(enricher, senders)
-    )
-  }
 }
--- src/app/Scheduler.scala
+++ src/app/Scheduler.scala
@@ -1,11 +1,35 @@
 package app
 
-case class Scheduler (enricher: Enricher, senders: List[MessageSender[_]]) {
+import app.models.{AicWeatherUpdate, AirlineWeatherUpdate, AtcWeatherUpdate, CanonicalWeatherUpdate}
+import com.rabbitmq.client.ConnectionFactory
+
+case class Scheduler(hostName: String, weatherApiUrl: String, onUpdate: MessageSender[_] => Unit) {
+  private val factory = new ConnectionFactory() {
+    setHost(hostName)
+  }
+  private val connection = factory.newConnection()
+  private val channel = connection.createChannel()
+  private val enricher = Enricher(weatherApiUrl)
+  private val senders = List(
+    MessageSender[AicWeatherUpdate](channel, "aic", new AicFilter, new AicTranslator),
+    MessageSender[AtcWeatherUpdate](channel, "atc", new AtcFilter, new AtcTranslator),
+    MessageSender[AirlineWeatherUpdate](channel, "sas", new AirlineFilter, new SasTranslator),
+    MessageSender[AirlineWeatherUpdate](channel, "klm", new AirlineFilter, new KlmTranslator),
+    MessageSender[AirlineWeatherUpdate](channel, "swa", new AirlineFilter, new XmlTranslator),
+    MessageSender[AirlineWeatherUpdate](channel, "baw", new AirlineFilter, new XmlTranslator),
+  )
+
   def start(timeoutInSeconds: Int): Unit = {
-    def onTimeout = {
+    def onTimeout: List[Unit] = {
       val weatherUpdate = enricher.enrich
-      senders.map(s => s.send(weatherUpdate))
+      senders.map(
+        s => {
+          s.send(weatherUpdate)
+          onUpdate(s)
+        }
+      )
     }
+
     Timer(timeoutInSeconds * 1000)(onTimeout)
   }
 }
