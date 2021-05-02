78694e02f540b266cddbd06da7dd2d87e5b8792c
Martin Hansen <m4h@outlook.dk>
2021-05-02 11:58:34 +0200

    Implement XmlTranslator, has faulty JAXB

--- src/app/Program.scala
+++ src/app/Program.scala
@@ -7,10 +7,10 @@ import scala.io.StdIn.readLine
 
 object Program extends App {
   val scheduler = Scheduler(
-    "localhost",
+    "192.168.10.90",
     "http://api.openweathermap.org/data/2.5/weather?zip=8260,dk&units=metric&appid=fb14f1139067845d2f30a3d147e7df89",
     s => printf(s.queueName)
   )
-  scheduler.start(300)
+  scheduler.start(5)
   readLine()
 }
--- src/app/XmlTranslator.scala
+++ src/app/XmlTranslator.scala
@@ -2,8 +2,16 @@ package app
 
 import app.models.AirlineWeatherUpdate
 
+import java.io.StringWriter
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.Marshaller
+
 class XmlTranslator extends Translator[AirlineWeatherUpdate, String]{
   override def apply(input: AirlineWeatherUpdate): String = {
-    throw new NotImplementedError()
+    val context = JAXBContext.newInstance(classOf[AirlineWeatherUpdate])
+    val marshaller = context.createMarshaller
+    val writer = new StringWriter()
+    marshaller.marshal(input, writer)
+    writer.toString
   }
 }
